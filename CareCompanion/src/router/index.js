801efdec543c6e26f16d76cd26caadd58edafa58
xunana <3520653564@qq.com>
2024-10-25 22:51:45 +0800

    feat:配置ui

--- CareCompanion/src/router/index.js
+++ CareCompanion/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter,createWebHashHistory, createWebHistory } from 'vue-router'
+import { createRouter,createWebHashHistory } from 'vue-router'
 import Layout from '../views/Main.vue'
 import Login from '../views/login/index.vue'
 const routes=[
@@ -14,7 +14,7 @@ const routes=[
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
   });
 
--- CareCompanion/src/views/login/index.vue
+++ CareCompanion/src/views/login/index.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        login
+       Login
     </div>
 </template>
