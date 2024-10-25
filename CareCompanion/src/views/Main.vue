c85b28363f0169f45204a00579a6db78c94406d3
xunana <3520653564@qq.com>
2024-10-25 23:14:31 +0800

    feat:aside组件创建

--- CareCompanion/src/views/Main.vue
+++ CareCompanion/src/views/Main.vue
@@ -1,10 +1,14 @@
 <template>
-    <div>
-        Layout    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
+    <div class="common-layout">
+      <el-container>
+        <el-header>Header</el-header>
+        <el-container>
+        <Aside/>
+          <el-main>Main</el-main>
+        </el-container>
+      </el-container>
     </div>
-</template>
+  </template>
+  <script setup>
+  import Aside from '../components/aside.vue'
+</script>
